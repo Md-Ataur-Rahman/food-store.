918af4ff490b24c751567c77f218cd9eeea0a64c
mdataurrahman555 <74402921+mdataurrahman555@users.noreply.github.com>
2022-03-26 10:48:04 +0600

    auto-selected worked

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,14 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import './main.css';
+import { Modal, Button } from 'react-bootstrap';
 import Product from '../Product/Product';
 
 const Main = () => {
     const [products, setProducts] = useState([]);
     const [addCart, setAddCart] = useState([]);
+    const [addCartModal, setModalCart] = useState([]);
+
+    // console.log(addCartModal);
+    
+     const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const addedCard = ( product ) => {
         let newAddedCard = [...addCart, product]
         setAddCart(newAddedCard);
+    }
+
+
+    const autoSelect = (  ) => {
+        let newNumOfCart = addCart[Math.floor(Math.random() * addCart.length)];
+        // console.log(newNumOfCart);
+        setModalCart(newNumOfCart);
     }
 
     useEffect(() => {
@@ -43,9 +58,43 @@ const Main = () => {
                     )) 
 
                 }
+
+                <button className='auto-choose-btn' onClick={() => {autoSelect(); handleShow();}}>Auto Choose</button>
+                <br></br>
+                <br></br>
+                <button className='reset-btn'>Reset</button>
             </div>
         </div>
 
+        <div>
+            
+{        /* <Button variant="primary" onClick={handleShow}>
+            Launch static backdrop modal
+        </Button> */}
+
+        <Modal
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+        >
+            <Modal.Header closeButton>
+            
+            </Modal.Header>
+            <Modal.Body>
+            <img src={addCartModal.img} alt="" className='modal-img'/> 
+            <h3>Name: {addCartModal.name}</h3>
+            <h4>Value: {addCartModal.value}</h4>
+            </Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+                Close
+            </Button>
+            </Modal.Footer>
+        </Modal>
+    
+        </div>
+        
     </div>
   )
 }
